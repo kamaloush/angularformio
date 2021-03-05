7658a7973a11bf9f07e9c3690d6de207baf4682d
Kamel Msaoubi <kmsaoubi@eudonet.com>
2021-03-05 12:37:19 +0100

    Cleaning and Added @formio/angular, module and tag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormioModule } from '@formio/angular';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    AppRoutingModule,
+    FormioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
